34c3382ba772e29d32dfd0012ff84afd67399817
mittalvaibhav1 <53215737+mittalvaibhav1@users.noreply.github.com>
2020-03-10 02:29:28 +0530

    Updated

--- js/script.js
+++ js/script.js
@@ -9,12 +9,10 @@ $( document ).ready(function() {
    if($(document).scrollTop()<x-20)
    {
     $('.side-nav').hide();
-    console.log("hell");
    }
    else{
     $('.side-nav').show();
    }
-   console.log("What");
 });
   function vis(){
     $('.side-nav').delay(1600).slideDown();
@@ -52,3 +50,125 @@ $( document ).ready(function() {
     }, speed);        
   })   
 });
+
+
+
+//Typewriter
+var app = document.getElementById('wel');
+
+var typewriter = new Typewriter(app, {
+    loop: false
+});
+
+typewriter.typeString('Welcome to IE(I) Student Chapter')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Welcome to IE(I) Student Chapter')
+    .start();
+
+          function init_map() {
+            var myOptions = {
+              zoom: 17,
+              center: new google.maps.LatLng(30.5160865, 76.659899999),
+              mapTypeId: google.maps.MapTypeId.ROADMAP,
+              styles: [
+                {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+                {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+                {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+                {
+                  featureType: 'administrative.locality',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#d59563'}]
+                },
+                {
+                  featureType: 'poi',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#d59563'}]
+                },
+                {
+                  featureType: 'poi.park',
+                  elementType: 'geometry',
+                  stylers: [{color: '#263c3f'}]
+                },
+                {
+                  featureType: 'poi.park',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#6b9a76'}]
+                },
+                {
+                  featureType: 'road',
+                  elementType: 'geometry',
+                  stylers: [{color: '#38414e'}]
+                },
+                {
+                  featureType: 'road',
+                  elementType: 'geometry.stroke',
+                  stylers: [{color: '#212a37'}]
+                },
+                {
+                  featureType: 'road',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#9ca5b3'}]
+                },
+                {
+                  featureType: 'road.highway',
+                  elementType: 'geometry',
+                  stylers: [{color: '#746855'}]
+                },
+                {
+                  featureType: 'road.highway',
+                  elementType: 'geometry.stroke',
+                  stylers: [{color: '#1f2835'}]
+                },
+                {
+                  featureType: 'road.highway',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#f3d19c'}]
+                },
+                {
+                  featureType: 'transit',
+                  elementType: 'geometry',
+                  stylers: [{color: '#2f3948'}]
+                },
+                {
+                  featureType: 'transit.station',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#d59563'}]
+                },
+                {
+                  featureType: 'water',
+                  elementType: 'geometry',
+                  stylers: [{color: '#17263c'}]
+                },
+                {
+                  featureType: 'water',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#515c6d'}]
+                },
+                {
+                  featureType: 'water',
+                  elementType: 'labels.text.stroke',
+                  stylers: [{color: '#17263c'}]
+                }
+              ]
+            };
+            map = new google.maps.Map(
+              document.getElementById("gmap_canvas"),
+              myOptions
+            );
+            
+            marker = new google.maps.Marker({
+              map: map,
+              animation: google.maps.Animation.BOUNCE,
+              position: new google.maps.LatLng(30.5160865, 76.659899999)
+            });
+            infowindow = new google.maps.InfoWindow({
+              content: "<strong>Chitkara University</strong><br>Chandigarh-Patiala National Highway (NH- 64), Village, Jansla, Rajpura, Punjab 140401<br> Punjab<br>"
+            });
+            google.maps.event.addListener(marker, "click", function () {
+              infowindow.open(map, marker);
+            });
+            infowindow.open(map, marker);
+          }
+          google.maps.event.addDomListener(window, "load", init_map);
+    
