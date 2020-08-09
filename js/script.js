291b9dbec9423cef33bcebc80ef48bdbcab2500b
mittalvaibhav1 <53215737+mittalvaibhav1@users.noreply.github.com>
2020-08-09 12:24:12 +0530

    Responsivness improved

--- js/script.js
+++ js/script.js
@@ -1,62 +1,59 @@
-/*Work in progress*/  
-$( document ).ready(function() {
-  /*document.location.href="#index";
-  $(".home").focus();*/
-  $('#mapwa').click(function(){
+
+//Sidenav Controller.
+$(document).ready(function () {
+  $('#mapwa').click(function () {
     $('.gm-fullscreen-control').click()
   });
   $('.side-nav').hide();
+
   var speed = 1600;
-  var x=$('.top').offset().top 
-  $(window).scroll(function() {
-    
-   if($(document).scrollTop()<x-20)
-   {
-    $('.side-nav').hide();
-   }
-   else{
-    $('.side-nav').show();
-   }
-   if($(document).width()<900)
-   {
-    $('.side-nav').hide();
-   }
-});
-  function vis(){
+  var x = $('.top').offset().top
+  $(window).scroll(function () {
+
+    if ($(document).scrollTop() < x - 20) {
+      $('.side-nav').hide();
+    } else {
+      $('.side-nav').show();
+    }
+    if ($(document).width() < 900) {
+      $('.side-nav').hide();
+    }
+  });
+
+  function vis() {
     $('.side-nav').delay(1600).slideDown();
   }
-  $("#arrow").click(function(){
-    $('html, body').stop().animate({ 
-      scrollTop: $('.top').offset().top 
+  $("#arrow").click(function () {
+    $('html, body').stop().animate({
+      scrollTop: $('.top').offset().top
     }, speed, vis());
-    
+
   })
 
+  $(".iei").click(function () {
+    $('html, body').stop().animate({
+      scrollTop: $('.top').offset().top
+    }, speed);
 
-  $(".iei").click(function(){
-    $('html, body').stop().animate({ 
-      scrollTop: $('.top').offset().top 
-    }, speed); 
- 
-  })
-  
-  $(".event").click(function(){
-    $('html, body').stop().animate({ 
-      scrollTop: $('.middle').offset().top 
-    }, speed);        
   })
 
-  $(".team").click(function(){
-    $('html, body').stop().animate({ 
-      scrollTop: $('.semimiddle').offset().top 
-    }, speed);        
+  $(".event").click(function () {
+    $('html, body').stop().animate({
+      scrollTop: $('.middle').offset().top
+    }, speed);
   })
-  
-  $(".contact").click(function(){
-    $('html, body').stop().animate({ 
-      scrollTop: $('.bottom').offset().top 
-    }, speed);        
-  })   
+
+  $(".team").click(function () {
+    $('html, body').stop().animate({
+      scrollTop: $('.semimiddle').offset().top
+    }, speed);
+  })
+
+  $(".contact").click(function () {
+    $('html, body').stop().animate({
+      scrollTop: $('.bottom').offset().top
+    }, speed);
+  })
 });
 
 
@@ -65,39 +62,39 @@ $( document ).ready(function() {
 var app = document.getElementById('wel');
 
 var typewriter = new Typewriter(app, {
-    loop: false
+  loop: false
 });
 
 typewriter.typeString('Welcome to IE(I) Student Chapter')
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString('Welcome to IE(I) Student Chapter')
-    .start();
+  .pauseFor(2500)
+  .deleteAll()
+  .typeString('Welcome to IE(I) Student Chapter')
+  .start();
 
-          function init_map() {
-            var myOptions = {
-              zoom: 17,
-              center: new google.maps.LatLng(30.5160865, 76.659899999),
-              mapTypeId: google.maps.MapTypeId.ROADMAP,
-              
-            };
-            map = new google.maps.Map(
-              document.getElementById("gmap_canvas"),
-              myOptions
-            );
-            
-            marker = new google.maps.Marker({
-              map: map,
-              animation: google.maps.Animation.BOUNCE,
-              position: new google.maps.LatLng(30.5160865, 76.659899999)
-            });
-            infowindow = new google.maps.InfoWindow({
-              content: "<strong>Chitkara University</strong><br>Chandigarh-Patiala National Highway (NH- 64), Village, Jansla, Rajpura, Punjab 140401<br> Punjab<br>"
-            });
-            google.maps.event.addListener(marker, "click", function () {
-              infowindow.open(map, marker);
-            });
-            infowindow.open(map, marker);
-          }
-          google.maps.event.addDomListener(window, "load", init_map);
-    
+//google maps
+function init_map() {
+  var myOptions = {
+    zoom: 17,
+    center: new google.maps.LatLng(30.5160865, 76.659899999),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+
+  };
+  map = new google.maps.Map(
+    document.getElementById("gmap_canvas"),
+    myOptions
+  );
+
+  marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.BOUNCE,
+    position: new google.maps.LatLng(30.5160865, 76.659899999)
+  });
+  infowindow = new google.maps.InfoWindow({
+    content: "<strong>Chitkara University</strong><br>Chandigarh-Patiala National Highway (NH- 64), Village, Jansla, Rajpura, Punjab 140401<br> Punjab<br>"
+  });
+  google.maps.event.addListener(marker, "click", function () {
+    infowindow.open(map, marker);
+  });
+  infowindow.open(map, marker);
+}
+google.maps.event.addDomListener(window, "load", init_map);
